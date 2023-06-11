968065868040fcdf6cd76e5d8e769f03190bbf90
nikef84 <nikzhadec@yandex.ru>
2023-06-11 12:58:49 +0300

    Init commit

--- firmware/project.mk
+++ firmware/project.mk
@@ -1,12 +1,25 @@
 # C source files
-PROJECT_CSRC 	= main.c
+PROJECT_CSRC 	= 	src/main.c \
+
+					
+					
+TESTSRC 	= 		
+
+					
+
 # C++ source files
 PROJECT_CPPSRC 	= 
 # Directories to search headers in
-PROJECT_INCDIR	= 
+PROJECT_INCDIR	= include \
+ 
+
 # Additional libraries
-PROJECT_LIBS	=
+PROJECT_LIBS	= -lm
 # Compiler options
-PROJECT_OPT     =
+PROJECT_OPT     = -DCHPRINTF_USE_FLOAT=1 -specs=nano.specs -specs=nosys.specs
 
 # Additional .mk files are included here
+CSRC = $(ALLCSRC) \
+       $(TESTSRC) \
+       $(PROJECT_CSRC) \
+       $(CHIBIOS)/os/various/evtimer.c
