cd7096458d00ecfca8b22251cedfe2d1bd16fd5d
nikef84 <nikzhadec@yandex.ru>
2023-06-21 23:07:14 +0300

    fix ack failure

--- firmware/tests/i2c/led_matrix_test.c
+++ firmware/tests/i2c/led_matrix_test.c
@@ -8,21 +8,20 @@ void led_matrix_test(void){
     halInit();
     chSysInit();
     debugStreamInit();
-    dbgPrintf("Start\r\n");
+    dbgPrintf("Start led matrix\r\n");
     i2cSimpleInit();
     chThdSleepMilliseconds(100);
-    //uint8_t buf[2] = {0x00, 0x00};
-    //i2cSimpleWrite(LM_ADDRESS, buf, 2);
+    uint8_t buf[2] = {0x00, 0x00};
+    i2cSimpleWrite(LM_ADDRESS, buf, 2);
     chThdSleepMilliseconds(100);
-    uint8_t tx[1] = {0x00};
-    uint8_t rx[1]  ={0x02};
     msg_t msg = MSG_OK;
-
+    uint8_t tx[2] = {0x02, 0x03};
+    msg = i2cSimpleWrite(LM_ADDRESS, tx, 2);
+    chThdSleepMilliseconds(100);
+	dbgPrintf("msg = %d\t err = %d\r\n", msg, i2cGetErrors(&I2CD1));
 
     while (true) {
     	palToggleLine(LINE_LED3);
-    	msg = i2cMasterTransmitTimeout(&I2CD1, LM_ADDRESS, tx, 1, rx, 1, 1000);
-    	dbgPrintf("msg = %d\t err = %d\r\n", msg, i2cGetErrors(&I2CD1));
     	chThdSleepMilliseconds(100);
     }
 }
